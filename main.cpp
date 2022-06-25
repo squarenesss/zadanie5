cde9cd30f1acb526e7a0fd1eb670d9981d71bddc
Mateusz Łepik <mateusz.lepik@gmail.com>
2022-06-25 21:10:38 +0200

    utworzenie header dla pliku

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+
+struct Header {
+    short Type;
+    int Size;
+    short Reserved1;
+    short Reserved2;
+    int BitsPosition;
+} File;
+
